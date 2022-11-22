b2e5da93a806c5cdeefd4f1d78f09d44d02358dd
latys <latys@LENOVO>
2022-11-22 20:44:58 +0300

    Добавьте файлы проекта.

---
+++ Logistic.WebUI/Controllers/RouteController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Logistic.DAL.Entities;
+using Logistic.DAL.Repositories.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Logistic.WebUI.Controllers
+{
+    public class RouteController : BaseController<IRouteRepository, Route>
+    {
+        private readonly IPointRepository _pointRepository;
+
+        public RouteController(IRouteRepository repository, IPointRepository pointRepository) : base(repository)
+        {
+            _pointRepository = pointRepository;
+        }
+
+        protected override Expression<Func<Route, bool>> SearchExpression(string searchString)
+        {
+            return route => route.Name.ToLower().Contains(searchString.ToLower().Trim());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string searchString, int? page)
+        {
+            var items = await GetSearchQuery(searchString)
+                .ToListAsync();
+
+            return View(ToPagedList(items, page));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await Repository.Delete(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var item = await Repository.GetEntityById(id);
+
+            var points = await _pointRepository.QueryEntities(includeAllChildren: false)
+                .ToListAsync();
+            ViewBag.Points = points;
+
+            return View(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Route item)
+        {
+            if (!ModelState.IsValid)
+            {
+                var points = await _pointRepository.QueryEntities(includeAllChildren: false)
+                    .ToListAsync();
+                ViewBag.Points = points;
+                return View(item);
+            }
+
+            await Repository.Update(item);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var points = await _pointRepository.QueryEntities(includeAllChildren: false)
+                .ToListAsync();
+            ViewBag.Points = points;
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Route item)
+        {
+            if (!ModelState.IsValid)
+            {
+                var points = await _pointRepository.QueryEntities(includeAllChildren: false)
+                    .ToListAsync();
+                ViewBag.Points = points;
+                return View(item);
+            }
+
+            await Repository.Add(item);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Logistic.WebUI/Controllers/WorkAnalysisController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Logistic.DAL.Repositories.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace Logistic.WebUI.Controllers
+{
+    [Route("[controller]")]
+    public class WorkAnalysisController : Controller
+    {
+        private readonly ITransportationRepository _transportationRepository;
+
+        public WorkAnalysisController(ITransportationRepository transportationRepository)
+        {
+            _transportationRepository = transportationRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("Stats")]
+        public async Task<IActionResult> GetStats(DateTime firstDate, DateTime secondDate)
+        {
+            secondDate = secondDate.AddHours(23);
+            var queryResult = await _transportationRepository.QueryEntities(includeAllChildren: false)
+                .Where(x => x.Date >= firstDate && x.Date <= secondDate)
+                .Include(t => t.Rate)
+                .Include(t => t.Cargo)
+                .Select(x => new
+                {
+                    TotalVolume = x.Cargo.Volume * x.CargoCount,
+                    TotalWeight = x.Cargo.Weight * x.CargoCount,
+                    TotalSum = x.Rate.CarryingRate * x.Cargo.Weight * x.CargoCount + x.Rate.VolumeRate * x.Cargo.Volume * x.CargoCount
+                })
+                .ToListAsync();
+
+            if (queryResult.Count == 0)
+            {
+                return Json("");
+            }
+
+            var totalSum = queryResult.Sum(x => x.TotalSum);
+            var totalVolume = queryResult.Sum(x => x.TotalVolume);
+            var totalWeight = queryResult.Sum(x => x.TotalWeight);
+
+            var totalResult = new
+            {
+                TotalSum = totalSum,
+                TotalVolume = totalVolume,
+                TotalWeight = totalWeight
+            };
+
+            var serializedResult = JsonConvert.SerializeObject(totalResult);
+
+            return Json(serializedResult);
+        }
+    }
+}
